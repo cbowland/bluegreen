fbe6226bed66a90fa937e3b60830a3bc4a757e28
cbowland <cbowland@gmail.com>
2019-06-19 18:30:54 -0500

    Update image.php

--- image.php
+++ image.php
@@ -10,6 +10,10 @@ imagefilledrectangle($im, 0, 0, 199, 199, $blue);
 //imagefilledrectangle($im, 0, 0, 199, 199, $green);
 
 // Save the image
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
 header('Content-Type: image/png');
 imagePNG($im);
 imagedestroy($im);
